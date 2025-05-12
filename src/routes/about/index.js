0151cb2d5db94781a04819a2932030c296a4bef0
PineappleLord2020 <mul20002@byui.edu>
2025-05-12 16:37:37 -0600

    Whelp, there goes the neighborhood

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -1,16 +1,14 @@
 import { Router } from 'express';
 
-import aboutData from '../../models/json/about.json' assert {type: "json"};
-
 const router = Router();
 
 router.get('/', (req, res) => {
     console.log(aboutData);
-    res.render('about/index', {title: aboutData.title, aboutData: aboutData});
+    res.render('about/index', {title: 'About - Infinity Train'});
 });
 
 router.get('/characters', (req, res) => {
-    res.render('about/characters', {title: 'Characters - Infinity Train'}, aboutData);
+    res.render('about/characters', {title: 'Characters - Infinity Train'});
 });
 
 router.get('/locations', (req, res) => {
