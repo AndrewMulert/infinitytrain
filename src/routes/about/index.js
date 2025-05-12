b0effd28cbc1a75874bbd02230cd249bb7471a55
PineappleLord2020 <mul20002@byui.edu>
2025-05-12 15:37:44 -0600

    I did a lot. Too bad it's broken for now

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 
+import aboutData from '../../models/json/about.json' assert {type: "json"};
+
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('about/index', {title: 'About - Infinity Train'});
+    console.log(aboutData);
+    res.render('about/index', {title: aboutData.title, aboutData: aboutData});
 });
 
 router.get('/characters', (req, res) => {
-    res.render('about/characters', {title: 'Characters - Infinity Train'});
+    res.render('about/characters', {title: 'Characters - Infinity Train'}, aboutData);
 });
 
 router.get('/locations', (req, res) => {
