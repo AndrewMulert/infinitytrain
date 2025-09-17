bf160f941f0b8cde3310485bb7df1eb8efbef751
PineappleLord2020 <mul20002@byui.edu>
2025-09-17 14:57:54 -0600

    Starting to set up the database

---
+++ src/models/About.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const aboutSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: [String],
+        required: true
+    },
+    image: {
+        src: { type: String, required: true },
+        alt: { type: String, required: true }
+    },
+    button: [{
+        text: { type: String },
+        href: { type: String },
+        target: { type: String }
+    }],
+    order: {
+        mediumView: {
+            row: Number,
+            column: Number
+        },
+        largeView: {
+            row: Number,
+            column: Number
+        },
+        position: {
+            type: Number
+        }
+    }
+},
+{collection: 'about'});
+
+const About = mongoose.model('About', aboutSchema);
+
+export default About;
---
+++ src/models/Merch.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const merchSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        src: { type: String, required: true },
+        alt: { type: String, required: true }
+    },
+    button: [{
+        text: { type: String },
+        href: { type: String },
+        target: { type: String }
+    }],
+    order: {
+        mediumView: {
+            row: Number,
+            column: Number
+        },
+        largeView: {
+            row: Number,
+            column: Number
+        },
+        position: {
+            type: Number
+        }
+    }
+},
+{collection: 'merch'});
+
+const Merch = mongoose.model('Merch', merchSchema);
+
+export default Merch;
