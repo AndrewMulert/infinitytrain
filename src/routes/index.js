1b8c00fc962b65781820da43256b53b6cdb9a1ee
PineappleLord2020 <mul20002@byui.edu>
2025-09-19 10:39:29 -0600

    Updated the home page to the database

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,27 @@
 import { Router } from 'express';
+import Home from '../models/Home.js';
 
 const router = Router();
  
 // The home page route
 router.get('/', async (req, res) => {
-    res.render('index', { 
-        title: 'Infinity Train', 
-        heroImage: 'index_hero_train_landscape', 
-        heroAlt: 'The Infinity Train spanning against the horizon, rendered by Andrew Mulert',
-        heroText: 'Climb Aboard the'
-    });
+    try {
+        const home = await Home.find().sort({_id: 1 });
+
+        console.log('Fetched Home:', home);
+        console.log('Number of Home:', home.length);
+        
+        res.render('index', { 
+            title: 'Infinity Train', 
+            heroImage: 'index_hero_train_landscape', 
+            heroAlt: 'The Infinity Train spanning against the horizon, rendered by Andrew Mulert',
+            heroText: 'Climb Aboard the',
+            home: home
+        });
+    } catch {
+        console.error('Error fetching about', err);
+        nextTick(err);
+    }
 });
 
 export default router;
